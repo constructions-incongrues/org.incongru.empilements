4fe76573cc8f1d42695737055812a283aa42099c
Tristan Rivoallan <tristan@rivoallan.net>
2011-10-13 15:50:50 +0200

    RSS link

--- var/www/bottom.php
+++ var/www/bottom.php
@@ -1,6 +1,6 @@
 	<hr style="clear:both;" />
 	<p>N'hésitez surtout pas à nous <a href="mailto:empilements@incongru.org">soumettre</a> vos compilations !</p>
-	<p class="credits">Ce projet est <a href="https://github.com/contructions-incongrues/empilements">développé</a> par <a href="http://www.constructions-incongrues.net">Constructions Incongrues</a> et hébergé par <a href="http://www.pastis-hosting.net">Pastis Hosting</a>.</p>
+	<p class="credits">Ce projet est <a href="https://github.com/contructions-incongrues/empilements">développé</a> par <a href="http://www.constructions-incongrues.net">Constructions Incongrues</a> et hébergé par <a href="http://www.pastis-hosting.net">Pastis Hosting</a> | <a href="http://feeds.feedburner.com/empilements-incongrus">RSS</a></p>
 	<br /><br /><br /><br /><br /><br />
 	
 	</div>
