14916f0243a67f7858170aaf6a041fc11c43e6e7
Tristan Rivoallan <tristan@rivoallan.net>
2021-10-15 19:44:03 +0000

    skin

--- src/public/index.php
+++ src/public/index.php
@@ -3,23 +3,21 @@
     <head>
         <title>Empilements | incongru.org</title>
         <script src="vendor/wimpy_core_7.84/wimpy.js"></script>
-        <style type="text/css" media="screen">
-            /* Make page fill device window */
-            html, body {
-                width:100%;
-                height:100%;
-                margin:0;
-                padding:0;
-            }
-        </style>
     </head>
 
     <body>
         <div class="wimpyContainer">
 
             <div
+                data-coverArtFit="3"
+                data-downloadEnable
+                data-media="wimpy.php?d=compilations"
+                data-responsive
+                data-skin="assets/wimpy/045.tsv"
+                data-startUpText="Empilements"
                 data-wimpyplayer 
-                data-media="wimpy.php?d=compilations">
+                style="width: 100%; height: 100%;"
+                >
             </div>
 
         </div>
