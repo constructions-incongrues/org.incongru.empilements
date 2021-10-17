b469c1cf6befd6f6321bfe298a825a16cc464f48
Tristan Rivoallan <tristan@rivoallan.net>
2021-10-17 04:46:19 +0000

    metadata

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,17 @@
 <html>
 
     <head>
+        <meta charset="utf-8">
         <title>Empilements | incongru.org</title>
+        <link rel="shortcut icon" type="image/png" href="assets/empilements_16x16.png" />
+    	<link rel="apple-touch-icon" href="assets/apple-touch-icon.png">
+        <link rel="alternate" type="application/rss+xml" title="Se tenir informer des nouvelles sorties" href="http://feeds.feedburner.com/empilements-incongrus">
+        <meta name="description" content="Aux fils de nos agapes, à la lueur d'un songe insomniaque, depuis les tréfonds de la nuit, le besoin impérieux de constituer des compilations musicales se fait parfois sentir.">
+        <meta property="og:title" content="Empilements | incongru.org" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="fr_fr" />
+        <meta property="og:description" content="Aux fils de nos agapes, à la lueur d'un songe insomniaque, depuis les tréfonds de la nuit, le besoin impérieux de constituer des compilations musicales se fait parfois sentir." />
+        <meta property="og:image" content="https://empilements.incongru.org/assets/logo.png" />
         <script src="vendor/wimpy_core_7.84/wimpy.js"></script>
     </head>
 
@@ -24,6 +34,17 @@
             </div>
 
         </div>
+
+        <footer>
+            <p>
+                Ce projet est développé par <a href="https://www.constructions-incongrues.net">Constructions Incongrues </a>
+                et est hébergé par <a href="https://www.pastis-hosting.net">Pastis Hosting</a>.
+            </p>
+
+            <p>
+                Le code source est <a href="https://github.com/constructions-incongrues/empilements">diffusé</a> sous license <a href="http://www.gnu.org/licenses/agpl-3.0.html">AGPLv3</a>.
+            </p>
+        </footer>
     </body>
 
 </html>
